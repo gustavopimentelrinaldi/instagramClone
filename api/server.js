dcf885a687088d3ad9eff17f1138d37ed63e1aca
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2018-03-23 16:07:51 -0300

    DB Connection

--- api/server.js
+++ api/server.js
@@ -12,9 +12,33 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017,{}),
+    {}
+);
+
 console.log("Servidor HTTP está executando na porta " + port);
 
 app.get('/', function(req, res){
     res.send({msg: 'Olá'});
 
-})
+});
+
+app.post('/api', function(req, res){
+    var dados = req.body;
+
+    db.open( function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.insert(dados, function(err, records){
+                if(err){
+                    res.json({'status' : '0'});
+                } else{
+                    res.json({'status' : '1'});
+                }
+
+                mongoclient.close();
+            })
+        })
+    } )
+});
